99740b77cb2ba6dd465f8a81fb0314c32e3f4197
asikalev <300783815@cencol.ca>
2018-02-10 17:38:00 -0500

    added createjs preloader

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -8,11 +8,20 @@
   let stage:createjs.Stage;
   let helloLabel: objects.Label;
   let clickMeButton: objects.Button;
+  let assetManager: createjs.LoadQueue;
+  let assetManifest: any[];
 
+  assetManifest = [
+    {id: "clickMeButton", src:"./Assets/image/clickMeButton.png"}
+  ];
+
+  //preloads assets
   function Init():void {
     console.log("Initialization Started...");
-
-    Start();
+    assetManager = new createjs.LoadQueue();
+    assetManager.installPlugin(createjs.Sound);
+    assetManager.loadManifest(assetManifest);
+    assetManager.on("complete", Start, this);
   }
 
   function Start():void {
@@ -43,9 +52,7 @@
     "Consolas", "#000000", 320, 240, true);
     stage.addChild(helloLabel);
 
-    clickMeButton = new objects.Button("./Assets/images/clickMeButton.png", 320, 340);
-    clickMeButton.regX = clickMeButton.getBounds().width * 0.5;
-    clickMeButton.regY = clickMeButton.getBounds().height * 0.5;
+    clickMeButton = new objects.Button(assetManager, "clickMeButton", 320, 340);
     stage.addChild(clickMeButton);
     clickMeButton.on("click", clickMeButtonClick);
 
